dbd9d174a0c84d10a39b18115ac5ae63830dbbd1
centos9 <2440580838@qq.com>
2025-04-15 16:58:13 +0800

    socket

---
+++ IO/nonBlockConnect.cpp
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <errno.h>
+#include <cstring>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/ioctl.h>
+
+#define BUFFER_SIZE 1023
+
+int setNonBlocking(int fd){
+    int old_option=fcntl(fd,F_GETFL);
+    int new_option=old_option|O_NONBLOCK;
+    fcntl(fd,F_SETFL,new_option);
+    return old_option;
+}
+
